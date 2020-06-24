40d0a9ef13d273a9f32f286a9e099080bf9d93d8
detnon <samueldetnon@gmail.com>
2020-06-24 15:55:32 +0100

    figured out authenticating with google API. TODO: Iterate through the different 'pages' of groups using the 'nextPageToken'

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import json
 import os
-
-# from pprint import pprint
+from pprint import pprint
 from typing import Optional
 
 import boto3  # type: ignore
@@ -12,13 +11,21 @@ client = boto3.client("ssm")
 
 
 def get_ssm_env_var() -> Optional[str]:
-    return os.environ.get("SSM_PARAMETER")
+    return os.environ.get("CREDENTIALS")
 
 
-def get_credentials_file(SSM_PARAMETER: Optional[str]) -> str:
-    content = client.get_parameter(Name=SSM_PARAMETER, WithDecryption=True)[
-        "Parameter"
-    ]["Value"]
+def get_subject_env_var() -> Optional[str]:
+    return os.environ.get("SUBJECT")
+
+
+def get_subject_email(SUBJECT: Optional[str]) -> str:
+    return client.get_parameter(Name=SUBJECT, WithDecryption=True)["Parameter"]["Value"]
+
+
+def get_credentials_file(CREDENTIALS: Optional[str]) -> str:
+    content = client.get_parameter(Name=CREDENTIALS, WithDecryption=True)["Parameter"][
+        "Value"
+    ]
 
     with open("credentials.json", "w") as outfile:
         outfile.write(content)
@@ -28,12 +35,22 @@ def get_credentials_file(SSM_PARAMETER: Optional[str]) -> str:
 SCOPES = ["https://www.googleapis.com/auth/admin.directory.group.readonly"]
 
 SERVICE_ACCOUNT_FILE = get_credentials_file(get_ssm_env_var())
+SUBJECT = get_subject_email(get_subject_env_var())
 
 credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES, subject=SUBJECT,
 )
 
-
-groups = googleapiclient.discovery.build(
-    "groupssettings", "v1", credentials=credentials,
+client = googleapiclient.discovery.build(
+    "admin", "directory_v1", credentials=credentials
 )
+
+response = (
+    client.groups()
+    .list(domain="digital.cabinet-office.gov.uk", maxResults=200)
+    .execute()
+)
+
+pprint(response)
+# for r in response:
+#     page_token = r["nextPageToken"]
