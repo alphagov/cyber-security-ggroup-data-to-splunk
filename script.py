92e38f7be1b7db1fcdf1f7ed1cdcc4eee06177c7
detnon <samueldetnon@gmail.com>
2020-06-25 12:13:12 +0100

    Typed existing code, and added everything to its own function. TODO: add a way to use the 'nextPageToken' to iterate through the pages and build a dictionary of group names and their ID's

--- script.py
+++ script.py
@@ -1,28 +1,28 @@
-import json
 import os
 from pprint import pprint
-from typing import Optional
+from typing import List, Optional
 
 import boto3  # type: ignore
 import googleapiclient.discovery  # type: ignore
 from google.oauth2 import service_account  # type: ignore
 
-client = boto3.client("ssm")
+
+def get_env_var(env) -> Optional[str]:
+    return os.environ.get(env)
 
 
-def get_ssm_env_var() -> Optional[str]:
-    return os.environ.get("CREDENTIALS")
-
-
-def get_subject_env_var() -> Optional[str]:
-    return os.environ.get("SUBJECT")
+def get_scope(SCOPE: Optional[str]) -> List[str]:
+    client = boto3.client("ssm")
+    return [client.get_parameter(Name=SCOPE, WithDecryption=True)["Parameter"]["Value"]]
 
 
 def get_subject_email(SUBJECT: Optional[str]) -> str:
+    client = boto3.client("ssm")
     return client.get_parameter(Name=SUBJECT, WithDecryption=True)["Parameter"]["Value"]
 
 
 def get_credentials_file(CREDENTIALS: Optional[str]) -> str:
+    client = boto3.client("ssm")
     content = client.get_parameter(Name=CREDENTIALS, WithDecryption=True)["Parameter"][
         "Value"
     ]
@@ -32,25 +32,39 @@ def get_credentials_file(CREDENTIALS: Optional[str]) -> str:
     return "credentials.json"
 
 
-SCOPES = ["https://www.googleapis.com/auth/admin.directory.group.readonly"]
+def get_groups(SERVICE_ACCOUNT_FILE: str, scope: List[str], subject: str) -> str:
+    client = boto3.client("ssm")
+    credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=scope, subject=subject,
+    )
 
-SERVICE_ACCOUNT_FILE = get_credentials_file(get_ssm_env_var())
-SUBJECT = get_subject_email(get_subject_env_var())
+    client = googleapiclient.discovery.build(
+        "admin", "directory_v1", credentials=credentials
+    )
 
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES, subject=SUBJECT,
-)
+    response = (
+        client.groups()
+        .list(domain="digital.cabinet-office.gov.uk", maxResults=10)
+        .execute()
+    )
 
-client = googleapiclient.discovery.build(
-    "admin", "directory_v1", credentials=credentials
-)
+    return response
 
-response = (
-    client.groups()
-    .list(domain="digital.cabinet-office.gov.uk", maxResults=200)
-    .execute()
-)
 
-pprint(response)
-# for r in response:
-#     page_token = r["nextPageToken"]
+def build_group_dict():
+    response = get_groups(
+        get_credentials_file(get_env_var("CREDENTIALS")),
+        get_scope(get_env_var("SCOPES")),
+        get_subject_email(get_env_var("SUBJECT")),
+    )
+
+    group_ids = {}
+
+    for r in response["groups"]:
+        group_names = r["name"]
+        group_id = r["id"]
+        group_ids[group_names] = group_id
+    return group_ids
+
+
+pprint(build_group_dict())
