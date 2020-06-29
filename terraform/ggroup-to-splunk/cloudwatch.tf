f5657514b3091846e9e4bce74fa27f110a852658
detnon <samueldetnon@gmail.com>
2020-06-29 17:00:39 +0100

    Subscribe log group to CSLS

--- terraform/ggroup-to-splunk/cloudwatch.tf
+++ terraform/ggroup-to-splunk/cloudwatch.tf
@@ -1,16 +1,16 @@
-resource "aws_cloudwatch_log_group" "google_group_data" {
-  name = "/aws/lambda/google-group-data"
+# resource "aws_cloudwatch_log_group" "google_group_data" {
+#   name = "/aws/lambda/google-group-data"
 
-  tags = {
-    Environment = var.Environment
-    Service     = var.Service
-  }
-}
-
-# resource "aws_cloudwatch_log_subscription_filter" "log_subscription" {
-#   name = "log_subscription"
-
-#   log_group_name  = "/aws/lambda/google-group-data"
-#   filter_pattern  = ""
-#   destination_arn = "arn:aws:logs:eu-west-2:885513274347:destination:csls_cw_logs_destination_prod"
+#   tags = {
+#     Environment = var.Environment
+#     Service     = var.Service
+#   }
 # }
+
+resource "aws_cloudwatch_log_subscription_filter" "log_subscription" {
+  name = "log_subscription"
+
+  log_group_name  = "/aws/lambda/send_ggroup_data_to_splunk"
+  filter_pattern  = ""
+  destination_arn = "arn:aws:logs:eu-west-2:885513274347:destination:csls_cw_logs_destination_prod"
+}
