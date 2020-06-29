568dee5006ca072c1166efe93b2675cc889bdc46
detnon <samueldetnon@gmail.com>
2020-06-29 10:46:58 +0100

    added variables.tf, forggoten from earlier commit

---
+++ terraform/ggroup-to-splunk/variables.tf
@@ -0,0 +1,43 @@
+variable "lambda_zip_location" {
+  description = "Location of the lambda zip"
+  type = string
+  default = ""
+  }
+
+variable "runtime" {
+  description = "runtime for lambda"
+  default     = "python3.7"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "lambda_memory" {
+  default = 2048
+}
+
+variable "lambda_timeout" {
+  default = 900
+}
+
+variable "Service" {
+  default = "ggroup-data-to-splunk"
+}
+
+variable "Environment" {
+  default = "test"
+}
+
+variable "SvcOwner" {
+  default = "cyber"
+}
+
+variable "DeployedUsing" {
+  default = "terraform"
+}
+
+variable "account_id" {
+  default = ""
+}
