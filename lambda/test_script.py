9176da7dc434d1564415c369f192b77092c7943c
alice-carr <alice.bc.carr@gmail.com>
2020-07-20 09:06:57 +0100

    add unit test for get credentials

--- lambda/test_script.py
+++ lambda/test_script.py
@@ -1,4 +1,6 @@
 from unittest.mock import Mock, patch
+import filecmp
+
 from script import *
 
 @patch('script.ssm_client.get_parameter')
@@ -14,3 +16,14 @@ def test_get_subject_email(mock_get_ssm):
     expected = "test_subject_email"
     actual = get_subject_email("test_email_param")
     assert expected == actual
+
+@patch('script.ssm_client.get_parameter')
+def test_get_credentials_file(mock_get_ssm):
+    mock_get_ssm.return_value = {"Parameter": {"Value": '{"test_creds": true}'}}
+    expected_credentials_file = "./test_assetts/expected_credentials_file.json"
+    actual_credentials_file = get_credentials_file("test_credentials_param", "./test_assetts/actual_credentials_file.json")
+
+    assert filecmp.cmp(actual_credentials_file, expected_credentials_file)
+
+    #clear test file
+    open(actual_credentials_file, 'w').close()
