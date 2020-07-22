3e31ba1a0942e658125dd47187aa323d62ca645c
alice-carr <alice.bc.carr@gmail.com>
2020-07-22 16:10:20 +0100

    change zip location

--- terraform/deployments/dev/main.tf
+++ terraform/deployments/dev/main.tf
@@ -1,7 +1,7 @@
 module "ggroup-to-splunk-dev" {
   source              = "../../modules/ggroup-to-splunk"
   environment         = "staging"
-  lambda_zip_location = "../../../lambda/get_data_for_google_groups.zip"
+  lambda_zip_location = "./lambda/get_data_for_google_groups.zip"
   runtime             = "python3.7"
   lambda_memory       = 256
   lambda_timeout      = 900
