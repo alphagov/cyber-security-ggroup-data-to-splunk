193f2360087a06c4d1071f50d01584feb854f717
alice-carr <alice.bc.carr@gmail.com>
2020-07-27 13:54:35 +0100

    add correct syntax

--- terraform/modules/ggroup-to-splunk/lambda_iam_policy.tf
+++ terraform/modules/ggroup-to-splunk/lambda_iam_policy.tf
@@ -1,41 +1,41 @@
 data "aws_iam_policy_document" "ggroup_to_splunk_lambda_policy" {
-    statement = {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:eu-west-2:${var.account_id}:*"
+    ]
+  }
+
+  statement {
       actions = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface"
       ]
       resources = [
-        "arn:aws:logs:eu-west-2:${var.account_id}:*"
+      "*"
       ]
-    }
+  }
 
-    statement = {
-       actions = [
-        "ec2:CreateNetworkInterface",
-        "ec2:DescribeNetworkInterfaces",
-        "ec2:DeleteNetworkInterface"
-       ]
-       resources = [
-        "*"
-       ]
-    }
-
-    statement = {
-      actions = [
-        "ssm:GetParameter",
-        "ssm:GetParameters",
-        "ssm:GetParametersByPath"  
-      ]
-      
-      resources = [
-        "arn:aws:ssm:eu-west-2:${var.account_id}:parameter/google_data_to_splunk/*"
-      ]
-    }
+  statement {
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath"  
+    ]
+    
+    resources = [
+      "arn:aws:ssm:eu-west-2:${var.account_id}:parameter/google_data_to_splunk/*"
+    ]
+  }
 }
 
 data "aws_iam_policy_document" "ggroup_to_splunk_lambda_trust" {
-  statement = {
+  statement {
     action = ["sts:AssumeRole"]
     principals {
       type        = "Service"
